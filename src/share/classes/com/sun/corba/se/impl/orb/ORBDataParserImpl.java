dcbfabdcef3514b5f6969cdbc2b209284e2287d8
Tomasz Adamski <tadamski@redhat.com>
2014-06-10 13:36:21 +0200

    When InitialHost/Port properties are not specified resolve_initial_references uses only local resolver and it is not delegated further

--- src/share/classes/com/sun/corba/se/impl/orb/ORBDataParserImpl.java
+++ src/share/classes/com/sun/corba/se/impl/orb/ORBDataParserImpl.java
@@ -57,7 +57,7 @@ public class ORBDataParserImpl extends ParserImplTableBase implements ORBData
     private ORB orb ;
     private ORBUtilSystemException wrapper ;
     private String ORBInitialHost ;
-    private int ORBInitialPort ;
+    private Integer ORBInitialPort ;
     private String ORBServerHost ;
     private int ORBServerPort ;
     private String listenOnAllInterfaces;
@@ -119,7 +119,7 @@ public class ORBDataParserImpl extends ParserImplTableBase implements ORBData
         return ORBInitialHost;
     }
 
-    public int getORBInitialPort()
+    public Integer getORBInitialPort()
     {
         return ORBInitialPort;
     }
